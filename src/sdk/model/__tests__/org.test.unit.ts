b107a9cc44073dfb0c1c8d9209df81eecd48b4cd
Nicolas FRADIN <fradinni@gmail.com>
2018-03-23 15:17:16 +0100

    Replace /orgs by /organizations in mocks and endpoints

--- src/sdk/model/__tests__/org.test.unit.ts
+++ src/sdk/model/__tests__/org.test.unit.ts
@@ -23,7 +23,7 @@ beforeAll(() => {
 test('Properly submits request to get Org', async() => {
   const uuid = 'test-org-uuid';
   return Org.getOrg(uuid).then(function(org) {
-    expect(Iland.getHttp().get).lastCalledWith(`/orgs/${uuid}`);
+    expect(Iland.getHttp().get).lastCalledWith(`/organizations/${uuid}`);
     expect(org.entityType).toBe('ORG');
   });
 });
@@ -31,7 +31,7 @@ test('Properly submits request to get Org', async() => {
 test('Properly submits request to get Orgs child vDCs', async() => {
   const org = new Org(MockOrgJson);
   return org.getVdcs().then(function(vdcs) {
-    expect(Iland.getHttp().get).lastCalledWith(`/orgs/${org.uuid}/vdcs`);
+    expect(Iland.getHttp().get).lastCalledWith(`/organizations/${org.uuid}/vdcs`);
     expect(vdcs.length).toBe(MockOrgVdcsJson.length);
     let idx = 0;
     for (const vdc of vdcs) {
@@ -44,7 +44,7 @@ test('Properly submits request to get Orgs child vDCs', async() => {
 test('Properly submits request to get Orgs child vApps', async() => {
   const org = new Org(MockOrgJson);
   return org.getVapps().then(function(vapps) {
-    expect(Iland.getHttp().get).lastCalledWith(`/orgs/${org.uuid}/vapps`);
+    expect(Iland.getHttp().get).lastCalledWith(`/organizations/${org.uuid}/vapps`);
     expect(vapps.length).toBe(MockOrgVappsJson.length);
     let idx = 0;
     for (const vapp of vapps) {
@@ -57,7 +57,7 @@ test('Properly submits request to get Orgs child vApps', async() => {
 test('Properly submits request to get Orgs child VMs', async() => {
   const org = new Org(MockOrgJson);
   return org.getVms().then(function(vms) {
-    expect(Iland.getHttp().get).lastCalledWith(`/orgs/${org.uuid}/vms`);
+    expect(Iland.getHttp().get).lastCalledWith(`/organizations/${org.uuid}/vms`);
     expect(vms.length).toBe(MockOrgVmsJson.length);
     let idx = 0;
     for (const vm of vms) {
@@ -70,7 +70,7 @@ test('Properly submits request to get Orgs child VMs', async() => {
 test('Properly submits request to get Orgs child edges', async() => {
   const org = new Org(MockOrgJson);
   return org.getEdges().then(function(edges) {
-    expect(Iland.getHttp().get).lastCalledWith(`/orgs/${org.uuid}/edges`);
+    expect(Iland.getHttp().get).lastCalledWith(`/organizations/${org.uuid}/edges`);
     expect(edges.length).toBe(MockOrgEdgesJson.length);
     let idx = 0;
     for (const edge of edges) {
@@ -83,7 +83,7 @@ test('Properly submits request to get Orgs child edges', async() => {
 test('Properly submits request to get Orgs child internal networks', async() => {
   const org = new Org(MockOrgJson);
   return org.getInternalNetworks().then(function(nets) {
-    expect(Iland.getHttp().get).lastCalledWith(`/orgs/${org.uuid}/vdc-networks`);
+    expect(Iland.getHttp().get).lastCalledWith(`/organizations/${org.uuid}/vdc-networks`);
     expect(nets.length).toBe(MockOrgInternalNetworksJson.length);
     let idx = 0;
     for (const net of nets) {
@@ -96,7 +96,7 @@ test('Properly submits request to get Orgs child internal networks', async() => 
 test('Properly submits request to get Orgs child vapp networks', async() => {
   const org = new Org(MockOrgJson);
   return org.getVappNetworks().then(function(nets) {
-    expect(Iland.getHttp().get).lastCalledWith(`/orgs/${org.uuid}/vapp-networks`);
+    expect(Iland.getHttp().get).lastCalledWith(`/organizations/${org.uuid}/vapp-networks`);
     expect(nets.length).toBe(MockOrgVappNetworksJson.length);
     let idx = 0;
     for (const net of nets) {
