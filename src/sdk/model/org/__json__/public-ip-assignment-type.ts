b4977e42efc592b6f7202e4c26af243e53a5c97a
Cory Snyder <snyder.135@wright.edu>
2018-06-07 16:27:22 -0400

    Resolves #102: add org public ip assignments endpoint (#115)

---
+++ src/sdk/model/org/__json__/public-ip-assignment-type.ts
@@ -0,0 +1,11 @@
+/**
+ * Org Public IP Assignment Type.
+ */
+export type PublicAssignmentType = 'VM' |
+    'VDC_EDGE' |
+    'VAPP_EDGE' |
+    'VIRTUAL_SERVER' |
+    'IPSEC_VPN' |
+    'SNAT' |
+    'DNAT' |
+    'UNASSIGNED';
