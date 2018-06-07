004edac8d00f1d1bb160fd5ad50aec9a3545411d
Cory Snyder <snyder.135@wright.edu>
2018-06-07 15:17:06 -0400

    Resolves #103: add org currency code endpoint (#113)

--- src/sdk/model/org/index.ts
+++ src/sdk/model/org/index.ts
@@ -33,3 +33,4 @@ export * from './nessus-scan-update-request';
 export * from './nessus-vulnerability';
 export * from './external-network';
 export * from './catalog-create-request';
+export * from './org-currency-code';
