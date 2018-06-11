f06a4c1c3883b6f7e4fd2238220de1968f635b75
Cory Snyder <snyder.135@wright.edu>
2018-06-11 14:47:46 -0400

    Resolves #145: add vdc storage profile summary endpoint method (#165)

--- src/sdk/model/vdc/__json__/index.ts
+++ src/sdk/model/vdc/__json__/index.ts
@@ -1,2 +1,4 @@
 export * from './vdc-allocation-model-type';
 export * from './vdc-json';
+export * from './storage-profile-bill-json';
+export * from './vdc-storage-profile-summary-json';
--- src/sdk/model/vdc/index.ts
+++ src/sdk/model/vdc/index.ts
@@ -1,3 +1,5 @@
 export * from './__json__/index';
 export * from './vdc-summary';
 export * from './vdc';
+export * from './vdc-storage-profile-bill';
+export * from './vdc-storage-profile-summary';
