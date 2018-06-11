fd8e184b9c8c69840582cbd7931fbc4d49be1ad5
Nicolas FRADIN <fradinni@gmail.com>
2018-06-11 12:05:48 +0200

    Resolves #125: Add VappNetworks get/update firewall endpoint

--- src/sdk/model/vapp-network/__json__/index.ts
+++ src/sdk/model/vapp-network/__json__/index.ts
@@ -2,3 +2,7 @@ export * from './vapp-network-json';
 export * from './vapp-network-update-request-json';
 export * from './vapp-network-dhcp-service-json';
 export * from './vapp-network-dhcp-service-update-request-json';
+export * from './vapp-network-firewall-json';
+export * from './vapp-network-firewall-rule-json';
+export * from './vapp-network-firewall-rule-request-json';
+export * from './vapp-network-firewall-update-request-json';
