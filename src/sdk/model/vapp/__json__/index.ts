6b681fd2acbf0df087ca5115df57fdc6af3128b1
Nicolas FRADIN <fradinni@gmail.com>
2018-06-13 18:12:49 +0200

    Resolves #182: Add vApp endpoint methods for getting and updating startup params

--- src/sdk/model/vapp/__json__/index.ts
+++ src/sdk/model/vapp/__json__/index.ts
@@ -3,3 +3,5 @@ export * from './vapp-json';
 export * from './vapp-resource-summary-json';
 export * from './vapp-rename-request-json';
 export * from './vapp-update-description-request-json';
+export * from './vapp-startup-section-item-json';
+export * from './vapp-startup-section-item-request-json';
