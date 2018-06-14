768a6a856152b97fae5ce8fc30fc291f20b109b0
Jonathan Masmejean <jonathan.masmejean@frigg-computing.com>
2018-06-14 17:05:56 +0200

    Add vm summary map to vapp endpoints.

--- src/sdk/model/vapp/__json__/index.ts
+++ src/sdk/model/vapp/__json__/index.ts
@@ -1,6 +1,5 @@
 export * from './vapp-status-type';
 export * from './vapp-json';
-export * from './vapp-resource-summary-json';
 export * from './vapp-rename-request-json';
 export * from './vapp-update-description-request-json';
 export * from './vapp-startup-section-item-json';
---
+++ src/sdk/model/vapp/summary/__json__/index.ts
@@ -0,0 +1,2 @@
+export * from './vapp-resource-summary-json';
+export * from './vm-ressource-summary-map-response-json';
