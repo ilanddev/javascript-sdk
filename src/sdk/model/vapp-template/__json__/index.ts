2cf59b0a542e53257177ece9d6f82d00852469d6
Nicolas FRADIN <fradinni@gmail.com>
2018-06-14 17:51:31 +0200

    Resolves #192: Add vApp template update/delete endpoint methods

--- src/sdk/model/vapp-template/__json__/index.ts
+++ src/sdk/model/vapp-template/__json__/index.ts
@@ -3,3 +3,4 @@ export * from './template-disk-configuration-json';
 export * from './template-vnic-configuration-json';
 export * from './vm-template-configuration-json';
 export * from './vapp-template-configuration-json';
+export * from './vapp-template-update-request-json';
