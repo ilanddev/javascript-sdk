872a461e73042eec7598e7fba313593f0674b2ce
nksprzak <nkasprzak@iland.com>
2018-06-15 10:32:08 -0500

    fix imports

--- src/sdk/model/edge/edge.ts
+++ src/sdk/model/edge/edge.ts
@@ -30,7 +30,8 @@ import { EdgeFirewallLogType } from './firewall/__json__/edge-firewall-log-type'
 import { EdgeBackingConfigurationType } from './__json__/edge-backing-configuration-type';
 import { EdgeInterfaceJson } from './edge-interface/__json__/edge-interface-json';
 import { EntityType } from '../common/__json__/entity-type';
-import { Task, TaskJson } from '../task';
+import { Task } from '..';
+import { TaskJson } from '../task/__json__/task-json';
 
 /**
  * Edge Gateway.
