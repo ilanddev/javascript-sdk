518baeefc44072e53c8c941757aab010cef75087
nksprzak <nkasprzak@iland.com>
2018-06-15 09:59:34 -0500

    remove indexes

--- src/sdk/model/company/support-ticket/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './__json__/index';
-export * from './support-ticket';
-export * from './support-ticket-create-request';
-export * from './support-ticket-update-request';
-export * from './support-ticket-attachment';
-export * from './support-ticket-comment';
