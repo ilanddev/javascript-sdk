37a97a6d1748ecb0a2b7fe4927f90cdb4c8010f4
Jonathan Masmejean <azazel971@gmail.com>
2018-06-18 19:07:16 +0200

    Resolves #223: refactor cloud tenant files

---
+++ src/sdk/model/vcc/cloud-tenant-resource/__json__/cloud-tenant-resource-json.ts
@@ -0,0 +1,8 @@
+import { CloudRepositoryJson } from '../../cloud-repository/__json__/cloud-repository-json';
+
+/**
+ * Interface for CloudTenantResource JSON properties.
+ */
+export interface CloudTenantResourceJson {
+  repository: CloudRepositoryJson;
+}
---
+++ src/sdk/model/vcc/vcc-perf-sample/__json__/vcc-perf-sample-json.ts
@@ -0,0 +1,8 @@
+/**
+ * Interface for Vcc Perf Sample Json properties.
+ */
+export interface VccPerfSampleJson {
+  used_quota: number;
+  quota: number;
+  time: number;
+}
