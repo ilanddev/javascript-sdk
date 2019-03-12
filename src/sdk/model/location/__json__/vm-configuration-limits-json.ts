fc54deea2848bf1ff864b31d6316c6951ea23b9f
Ari Quinones <ariquinones0015@gmail.com>
2019-03-12 11:20:05 -0400

    add vm configuration limits endpoint (#406)

---
+++ src/sdk/model/location/__json__/vm-configuration-limits-json.ts
@@ -0,0 +1,8 @@
+export interface VmConfigurationLimitsJson {
+  max_memory_mebibytes: number;
+  min_memory_mebibytes: number;
+  max_cpu_core_count: number;
+  max_disk_size_mebibytes: number;
+  min_disk_size_mebibytes: number;
+}
+
