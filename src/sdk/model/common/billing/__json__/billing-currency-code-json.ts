875696c7de862951b52d69a00c8b6a9d4da1fc8c
Jonathan Masmejean <azazel971@gmail.com>
2020-03-13 15:58:40 +0100

    Add vac billing currency code endpoint

---
+++ src/sdk/model/common/billing/__json__/billing-currency-code-json.ts
@@ -0,0 +1,8 @@
+import { CurrencyCode } from './currency-code-type';
+
+/**
+ * Interface for billing currency code JSON representation
+ */
+export interface BillingCurrencyCodeJson {
+  currency_code: CurrencyCode;
+}
