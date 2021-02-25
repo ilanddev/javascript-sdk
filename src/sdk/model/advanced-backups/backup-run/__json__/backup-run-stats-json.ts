9c41e5a45d1bef4e3bc925d621123eae322fda0e
Natasha Chernis <nchernis@iland.com>
2021-02-24 18:22:17 -0600

    change startTimeUsecs property name to startTimeMillis in BackupRunStats model

--- src/sdk/model/advanced-backups/backup-run/__json__/backup-run-stats-json.ts
+++ src/sdk/model/advanced-backups/backup-run/__json__/backup-run-stats-json.ts
@@ -7,7 +7,8 @@ export interface BackupRunStatsJson {
   num_canceled_tasks: number;
   num_failed_tasks: number;
   num_successful_tasks: number;
-  start_time_usecs: number;
+  start_time_millis: number;
+  start_time_usecs: number; // deprecated
   time_taken_millis?: number;
   total_bytes_read_from_source: number;
   total_bytes_to_read_from_source: number;
