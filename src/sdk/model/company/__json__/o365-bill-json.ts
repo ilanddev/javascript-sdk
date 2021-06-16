3c09345d78b485d03217c2407b698983f725c240
Julio Chazari <jchazari@JCHAZARI-MBP.attlocal.net>
2021-06-16 15:57:57 -0500

    Add burst count licenses & total cost for used licenses fields to O365 Bill model

--- src/sdk/model/company/__json__/o365-bill-json.ts
+++ src/sdk/model/company/__json__/o365-bill-json.ts
@@ -4,12 +4,14 @@ import { CurrencyCode } from '../../common/billing/__json__/currency-code-type';
  * O365 Bill Response JSON properties
  */
 export interface O365BillJson {
+  burst_count_licenses: number;
   cost_per_license: number;
   currency_code: CurrencyCodeResponse;
   full_month_contract_cost: number;
   month: number;
   number_of_licenses: number;
   total: number;
+  total_cost_for_used_licenses: number;
   year: number;
 }
 
