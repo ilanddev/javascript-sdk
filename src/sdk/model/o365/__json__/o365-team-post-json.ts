3528cc83524c4c7b18a6289beef42a7e2b4e4289
Julio Chazari <jchazari@JCHAZARI-MBP.attlocal.net>
2021-09-02 13:21:38 -0500

    Add get teams data (channels, posts, files, tabs) for O365 restore session

---
+++ src/sdk/model/o365/__json__/o365-team-post-json.ts
@@ -0,0 +1,11 @@
+/**
+ * O365 Team Post JSON properties
+ */
+export interface O365TeamPostJson {
+  author: string;
+  created_time: number;
+  is_important: boolean;
+  last_modification_time: number;
+  native_id: string;
+  subject: string;
+}
